3127c84d657334d4ed52ded28a89be16d1d3de6e
duaraghav8@gmail <duaraghav8@gmail.com>
2015-12-06 10:41:08 +0530

    Subset Sum C++

---
+++ subset_sum/C++/subset.cpp
@@ -0,0 +1,38 @@
+/*
+	Subset Sum Problem - Determines whether a possible subset of the given set exists such that the sum of elements of the subset = target
+	Time Complexity: O(nlog (n))
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool subset_sum (int target, vector< int > numbers, int vector_size) {
+	if (!target) {
+		return (true);
+	}
+	if (!vector_size && target) {
+		return (false);
+	}
+	return (subset_sum (target, numbers, vector_size - 1) || subset_sum (target - numbers [vector_size - 1], numbers, vector_size - 1));
+}
+
+int main () {
+	vector< int > numbers;
+	int target (0), size (0);
+
+	cout << "Size: "; cin >> size;
+	cout << "Numbers (space separated): ";
+	for (int i = 0; i < size; i++) {
+		int buf (0);
+		cin >> buf;
+		numbers.push_back (buf);
+	}
+	cout << "Target: "; cin >> target;
+
+	sort (numbers.begin (), numbers.end ());
+	cout << (subset_sum (target, numbers, numbers.size ()) ? "YES!" : "NO..") << endl;
+
+	return (0);
+}
