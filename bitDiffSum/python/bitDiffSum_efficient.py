767a0dfd6585f8ac6c1dce597c4d6ef5c8c79439
duaraghav8@gmail <duaraghav8@gmail.com>
2016-02-09 21:22:21 +0530

    Bit Difference sum

--- bitDiffSum/python/bitDiffSum_efficient.py
+++ bitDiffSum/python/bitDiffSum_efficient.py
@@ -1,24 +1,21 @@
 ##########################################################################################
 #http://www.geeksforgeeks.org/sum-of-bit-differences-among-all-pairs/
-#A naive solution for problem of sum of bit differences amongst all pairs
-#Time Complexity: O (N^2)
+#An efficient solution for problem of sum of bit differences amongst all pairs
+#Time Complexity: O (N)
 ##########################################################################################
 
-from itertools import combinations as comb;
+def countForBit (array, bit):
+	bitSetCount = 0;
+	mask = 1 << bit;
 
-def bitDiffCount (x, y):
-	count, mask = 0, 1;
-	for i in range (32):
-		if (not ( (x & mask) == (y & mask))):
-			count += 1;
-		x = x >> 1;
-		y = y >> 1;
-	return (count*2);
+	for number in array:
+		if (number & mask):	bitSetCount += 1;
+	return (bitSetCount * (len (array)-bitSetCount) * 2);
 
 def bitDiff (array):
 	count = 0;
-	for pair in comb (array, 2):
-		count += bitDiffCount (pair [0], pair [1]);
+	for bit in range (0, 32):
+		count += countForBit (array, bit);
 	return (count);
 
 if (__name__ == '__main__'):
